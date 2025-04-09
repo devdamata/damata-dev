a7888d8e863b47c21742016282a680d51b8ff686
damata12310 <anderson.daamata@gmail.com>
2025-04-09 00:53:30 -0300

    First  commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Document</title>
+</head>
+<body>
+<div class="container mx-auto">
+    <h1 class="text-3xl font-bold underline text-red-500">
+        Hello world!
+    </h1>
+    <p class="mt-4">This is a simple Laravel application using Tailwind CSS.</p>
+</body>
+</html>
